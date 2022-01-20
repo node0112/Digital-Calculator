c879224f0c57dac3dd2ecc9b64a222354a339447
node0112 <siddheshthorat25@gmail.com>
2022-01-20 12:20:53 +0100

    Corrected calculator logic

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const numButtons=document.querySelectorAll('.number')
 const opButtons=document.querySelectorAll('.buttons')
 const dis=document.querySelector('.display-text')
+let displayVal
 let val
 let num
 let clckcount=0
-var val1,val2
+var val1="",val2=""
 let op
 let ans
 
@@ -14,17 +15,19 @@ numButtons.forEach(button =>{
        button.style.color="gray"
        setTimeout(function (){ boxShadow(val); }, 150)
        val="num"
-       clckcount=clckcount+1
+       if(clckcount==0){
+           clckcount=1
+       }
        num=button.textContent
-       displayWrite(num)
-       if(clckcount==1){
-           val1=parseInt(num)
-       }
-       if(clckcount==3){
-           val2=parseInt(num)
-           clckcount=4
-           calculate(val1,val2,op)
-       }
+        if(clckcount==1){
+           val1=val1+(num)
+           displayWrite(val1)
+        }
+        if(clckcount==2||clckcount==3){
+            clckcount=3
+            val2=val2+num
+            displayWrite(val2)
+        }
     })
 })
 
@@ -46,6 +49,9 @@ opButtons.forEach(button =>{
        if(num=="CLEAR"){
            num=0
            clckcount=0
+           val1=''
+           val2=''
+           op=''
            displayWrite(num)
        }
        if(num=="="){
@@ -57,7 +63,8 @@ opButtons.forEach(button =>{
 })
 
 function calculate(val1,val2,op){
-    console.log(op)
+    val1=parseInt(val1)
+    val2=parseInt(val2)
     if(op=="+"){
        ans=val1+val2
        clckcount=0
@@ -80,9 +87,13 @@ function calculate(val1,val2,op){
     }
 }
 
+//////////////////DISPLAY NUMBERS\\\\\\\\\\\\\\\\
 function displayWrite(num){
-     dis.textContent=num
-     console.log(num)
+    displayVal=num
+    if(clckcount==11||clckcount==33){
+      displayVal=displayVal+num
+    }
+     dis.textContent=displayVal
 }
 
 function boxShadow(val){
