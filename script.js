34e0f0b6a29f91a8097cf91d200a97f95e97c76e
node0112 <siddheshthorat25@gmail.com>
2022-01-22 11:41:26 +0100

    imporved UI and links

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const numButtons=document.querySelectorAll('.number')
 const opButtons=document.querySelectorAll('.buttons')
 const dis=document.querySelector('.display-text')
 const headerLinks=document.querySelectorAll('a')
+const header=document.querySelector('.header-container')
+const logo=document.querySelector('.left-logo')
 let displayVal
 let val
 let num
 let clckcount=0
 var val1="",val2=""
-let op
-let ans
+let op=''
+var ans
+let opState=0
+let opTimes=0
 
-headerLinks.forEach(link=>{
+headerLinks.forEach(link=>{// appearance enhancement
     link.addEventListener('mouseover', ()=>{
         link.style.textShadow=" 0 0 30px yellow"
     })
@@ -18,78 +22,119 @@ headerLinks.forEach(link=>{
         link.style.textShadow="none"
     })
 })
+
+logo.addEventListener('mouseover', ()=>{//more appearance enhancement
+    logo.style.color="red"
+})
+logo.addEventListener('mouseout',()=>{
+    logo.style.color="rgb(232, 255, 27)"
+})
+
 numButtons.forEach(button =>{
     button.addEventListener('click', ()=>{
+        header.style.boxShadow="0 0 30px rgb(255, 31, 31)"
        button.style="box-shadow: none;"
        button.style.color="gray"
        setTimeout(function (){ boxShadow(val); }, 150)
        val="num"
-       if(clckcount==0){
-           clckcount=1
-           val1=val1
-           val2=''
-           op=''
-       }
        num=button.textContent
+       if(clckcount==0){
+           val1=val1+num
+           displayWrite(val1)
+       }
         if(clckcount==1){
            val1=val1+(num)
            displayWrite(val1)
         }
-        if(clckcount==2||clckcount==3){
+        if(opTimes==1||clckcount==2){
             clckcount=3
             val2=val2+num
             displayWrite(val2)
+            opState=1
         }
     })
 })
 
 opButtons.forEach(button =>{
     button.addEventListener('click', ()=>{
+       header.style.boxShadow="0 0 30px rgb(255, 31, 31)"
        button.style="box-shadow: none;"
        button.style.color="gray"
        val="op"
        num=button.textContent
        if(num=="+"||num=="-"||num=="x"||num=="÷"){
-           op=num
-           clck=2
+            if(opState==0 && opTimes==0){ //default first time operation is performed
+               op=num 
+               displayWrite(op)
+               opState=1
+               opTimes=1
+               clckcount=2
+           }
+           else if(opState==1 && opTimes>0){
+            calculate(val1,val2,op)
+            val1=ans
+            op=num
+            opState=1
+            val2=''
+            clckcount=2
+           }
+           else if(opState==0 && opTimes==1){
+               val2=''
+               val1=ans
+               clckcount=2
+               op=num
+               
+           }
        }
        setTimeout(function (){ boxShadow(val); }, 150)
-       if(num!="="){
-       displayWrite(num)
-       clckcount=2
+       if(num=="="){
+           equal()
        }
        if(num=="CLEAR"){
-           num=0
-           clckcount=0
-           val1=''
-           val2=''
-           op=''
-           displayWrite(num)
-       }
-       if(num=="="){
-           console.log(clckcount)
-           clckcount=4
-           calculate(val1,val2,op)
-       }
+           clear()
+    }
     })
 })
 
+function clear(){
+        num=0
+        clckcount=0
+        val1=''
+        val2=''
+        op=''
+        ans=0
+        opTimes=0
+        opState=0
+        displayWrite(num)
+}
+function equal(){
+    header.style.boxShadow="0 0 30px green"
+    if(val2=='' || val1==''|| op==''){
+        displayWrite("error")
+        setTimeout(function () {clear()},1000)
+        header.style.boxShadow="0 0 30px yellow"
+    }
+    else{
+    console.log(val1,op,val2)
+    calculate(val1,val2,op)
+    opTimes=1
+    opState=0
+    }
+}
+
 function calculate(val1,val2,op){
-    val1=parseInt(val1)
-    val2=parseInt(val2)
+    val1=parseFloat(val1)
+    val2=parseFloat(val2)
     if(op=="+"){
        ans=val1+val2
-       clckcount=0
        displayWrite(ans)
     }
     if(op=="-"){
       ans=val1-val2
-      clckcount=0
        displayWrite(ans)
     }
     if(op=="x"){
       ans=val1*val2
-      clckcount=0
        displayWrite(ans)
     }
     if(op=="÷"){
@@ -98,8 +143,7 @@ function calculate(val1,val2,op){
             displayWrite(ans)
         }
         else{
-      ans=val1/val2
-      clckcount=0
+      Math.floor((ans=val1/val2))
        displayWrite(ans)
     }
     }
